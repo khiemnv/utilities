5ddfa1e71c7f0d4c76d441d12bfe69c6c18450a5
khiemnv88 <ra4242it@yahoo.com.vn>
2020-03-29 11:58:46 +0900

    HEIC to jpg

---
+++ HEICConverter/Program.cs
@@ -0,0 +1,30 @@
+﻿using ImageMagick;
+using System;
+using System.IO;
+
+namespace HEICConverter
+{
+    class Program
+    {
+        public static void ProcessFile(string path)
+        {
+            using (MagickImage image = new MagickImage(path))
+            {
+                string newFile = path.Replace(Path.GetExtension(path), ".jpg");
+                image.Write(newFile);
+            }
+            Console.WriteLine("Processed file '{0}'.", path);
+        }
+        static void Main(string[] args)
+        {
+            foreach (string path in args)
+            {
+                if (File.Exists(path))
+                {
+                    ProcessFile(path);
+                }
+            }
+        }
+    }
+
+}
