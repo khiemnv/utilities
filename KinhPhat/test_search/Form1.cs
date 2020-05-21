0239e747351587a9eddb3f72dbd2c49c9da52369
khiemnv88 <ra4242it@yahoo.com.vn>
2020-05-22 00:31:04 +0900

    test search

---
+++ KinhPhat/test_search/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using WindowsFormsApp1;
+
+namespace test_search
+{
+    public partial class Form1 : Form
+    {
+        SearchPanel m_srchPanel;
+        public Form1()
+        {
+            InitializeComponent();
+            m_srchPanel = new SearchPanel(cnnStr);
+            m_srchPanel.OnSelectTitle += (s, e) =>
+            {
+                Debug.WriteLine("titleId {0}", e);
+            };
+            this.Controls.Add(m_srchPanel.m_tblLayout);
+        }
+
+        string cnnStr
+        {
+            get
+            {
+                string srcDb = @"D:\tmp\github\utilities\KinhPhat\kinhtang_test.accdb";
+                var cnnStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=<db>;";
+                return cnnStr.Replace("<db>", srcDb);
+            }
+        }
+
+        void testHeap()
+        {
+            int[] arr = new int[10];
+            var rnd = new Random(101);
+            for (int i = 0; i < 10; i++)
+            {
+                arr[i] = rnd.Next(1, 100);
+            }
+            Func<int, int, int> cmp = (x, y) => x - y;
+            var h = new myHeap<int>(arr, cmp);
+            for (int i = 0; i < 10; i++)
+            {
+                var min = h.PopMin();
+            }
+        }
+    }
+
+}
