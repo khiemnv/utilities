f3864e876ba033327398099d1aa091590eab64f5
khiemnv88 <ra4242it@yahoo.com.vn>
2020-05-24 03:33:06 +0900

    display search result by browser; handle click on title in browser;

--- KinhPhat/WindowsFormsApp1/Searcher.cs
+++ KinhPhat/WindowsFormsApp1/Searcher.cs
@@ -1,11 +1,16 @@
 ﻿#define use_sqlite
+#define use_browser
 
 using System;
 using System.Collections.Generic;
 using System.Data.OleDb;
 using System.Data.SQLite;
 using System.Diagnostics;
+using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -17,6 +22,8 @@ namespace WindowsFormsApp1
     {
         string m_cnnStr;
         string m_searchDb;
+        SQLiteConnection m_sqliteCnn;
+        OleDbConnection m_cnn;
 
         public Searcher(string cnnStr, string srchDb = "")
         {
@@ -27,15 +34,27 @@ namespace WindowsFormsApp1
                 var m = reg.Match(cnnStr);
                 m_searchDb = m.Groups[1].Value.Replace(".accdb", "_search.db");
             }
+            m_cnn = new OleDbConnection(m_cnnStr);
+            m_cnn.Open();
+            m_sqliteCnn = new SQLiteConnection(sqliteCnnStr);
+            m_sqliteCnn.Open();
+        }
+
+        public void Close()
+        {
+            m_cnn.Close();
+            m_sqliteCnn.Close();
+            m_cnn.Dispose();
+            m_sqliteCnn.Dispose();
+        }
+
+        ~Searcher()
+        {
         }
 
         public void BuildSearchDb()
         {
-            var cnn = new OleDbConnection(m_cnnStr);
-            cnn.Open();
-            var cmd = new OleDbCommand();
-            cmd.Connection = cnn;
-            cmd.CommandText = "SELECT * FROM paragraphs";
+            var cmd = new OleDbCommand("SELECT * FROM paragraphs", m_cnn);
             //cmd.CommandText = "SELECT * FROM paragraphs WHERE ID > (SELECT MAX(paragraphId) FROM words)";
             cmd.CommandText = "SELECT * FROM paragraphs WHERE ID > 42207";  //last build search data
             var rd = cmd.ExecuteReader();
@@ -76,9 +95,6 @@ namespace WindowsFormsApp1
 
             //insert to word tbl
             AddToSearchDb(dict, lst);
-
-            cnn.Close();
-            cnn.Dispose();
         }
 
         void insertToDb(OleDbConnection cnn, Dictionary<string, UInt64> keysD, List<word> wordsL)
@@ -137,16 +153,18 @@ namespace WindowsFormsApp1
             trans.Dispose();
         }
 
-        string sqliteCnnStr { get
+        string sqliteCnnStr
+        {
+            get
             {
-                return string.Format(@"Data Source={0};version=3;",m_searchDb);
-            } }
+                return string.Format(@"Data Source={0};version=3;", m_searchDb);
+            }
+        }
 
         void AddToSearchDb(HashSet<string> keysH, List<word> wordsL)
         {
             //find keys
-            var cnn = new SQLiteConnection(sqliteCnnStr);
-            cnn.Open();
+            var cnn = m_sqliteCnn;
             var cmd1 = new SQLiteCommand("select ID from keys where key = ?", cnn);
             cmd1.Parameters.Add(new SQLiteParameter(System.Data.DbType.String));
             var keysL = new List<string>();
@@ -199,8 +217,6 @@ namespace WindowsFormsApp1
             cmd3.Dispose();
             trans.Commit();
             trans.Dispose();
-            cnn.Close();
-            cnn.Dispose();
         }
 
         static Dictionary<string, string> dict = new Dictionary<string, string>() {
@@ -276,13 +292,54 @@ namespace WindowsFormsApp1
             }
         }
 
-        public SrchRes[] Find(string txt)
+        public SrchResult Find(string txt)
         {
             int nRow;
             var arr = getWords(txt, out nRow);
 
-            var ret = calcDiff(arr, nRow);
-            return ret;
+            var recs = calcDiff(arr, nRow);
+
+            var h = new HashSet<UInt64>();
+            var parH = new HashSet<UInt64>();
+            foreach (var res in recs)
+            {
+                h.Add(res.detail[0].titleId);
+                foreach (var w in res.detail)
+                {
+                    parH.Add(w.parId);
+                }
+            }
+            var titles = h.Select(titleId => getTitleInfo(titleId)).ToList();
+            var paragraphs = parH.Select(parId => getParContent(parId)).ToList();
+            return new SrchResult()
+            {
+                titles = titles,
+                paragraphs = paragraphs,
+                recs = recs
+            };
+        }
+
+        MyTitle getTitleInfo(UInt64 id)
+        {
+            var cmd = new OleDbCommand(string.Format("select title from titles where id = {0}", id), m_cnn);
+            var zTitle = cmd.ExecuteScalar().ToString();
+            cmd.Dispose();
+            return new MyTitle()
+            {
+                ID = id,
+                zTitle = zTitle,
+            };
+        }
+        MyParagraph getParContent(UInt64 id)
+        {
+            var cmd = new OleDbCommand(string.Format("select content from paragraphs where id = {0}", id), m_cnn);
+            var content = cmd.ExecuteScalar().ToString();
+            cmd.Dispose();
+            return new MyParagraph()
+            {
+                ID = id,
+                content = content
+            };
         }
 
         class MyTimer
@@ -401,13 +458,13 @@ namespace WindowsFormsApp1
         }
 #endif
 
-        SrchRes[] calcDiff(word[][] arr, int nRow)
+        SrchRec[] calcDiff(word[][] arr, int nRow)
         {
             var begin = Environment.TickCount;
-            var res = new SrchRes[arr[0].Length];
+            var res = new SrchRec[arr[0].Length];
             for (int i = 0; i < arr[0].Length; i++)
             {
-                res[i] = new SrchRes() { path = new int[] { i },d = 0};
+                res[i] = new SrchRec() { path = new int[] { i }, d = 0 };
             }
             for (var row = 1; row < nRow; row++)
             {
@@ -427,20 +484,20 @@ namespace WindowsFormsApp1
                             //var d = wordDiff(res[j].w, lst[k]);
                             var d = Convert.ToInt32(prevW.parId) - Convert.ToInt32(curW.parId);
                             d *= d < 0 ? -100 : 100;
-                            d += Math.Abs(prevW.pos - curW.pos)*10;
+                            d += Math.Abs(prevW.pos - curW.pos) * 10;
                             d += prevD;
                             tmplRes.Add(new int[] { j, k, d });
                         }
                     }
                 }
-                
+
                 var h = new myHeap<int[]>(tmplRes.ToArray(), (x, y) => x[2] - y[2]);
                 var n = Math.Min(100, tmplRes.Count);
-                var top100 = new SrchRes[100];
+                var top100 = new SrchRec[100];
                 for (int i = 0; i < n; i++)
                 {
                     var t = h.PopMin();
-                    top100[i] = new SrchRes() { d = t[2], path = new int[row + 1]};
+                    top100[i] = new SrchRec() { d = t[2], path = new int[row + 1] };
                     res[t[0]].path.CopyTo(top100[i].path, 0);
                     top100[i].path[row] = t[1];
                 }
@@ -448,7 +505,7 @@ namespace WindowsFormsApp1
             }
             var elapsed = Environment.TickCount - begin;
             Debug.WriteLine("calc diff {0}", elapsed);
-            foreach(var item in res)
+            foreach (var item in res)
             {
                 var row = 0;
                 item.detail = item.path.Select(v => arr[row++][v]).ToList();
@@ -476,7 +533,13 @@ namespace WindowsFormsApp1
         }
     }
 
-    public class SrchRes
+    public class SrchResult
+    {
+        public SrchRec[] recs;
+        public List<MyTitle> titles;
+        public List<MyParagraph> paragraphs;
+    }
+    public class SrchRec
     {
         public int[] path;
         public int d;
@@ -556,7 +619,20 @@ namespace WindowsFormsApp1
     {
         string cnnStr;
         public TableLayoutPanel m_tblLayout;
+#if use_browser
+        class JsHandler
+        {
+            public EventHandler<int> OnTitleSelected;
+            public void HandleJsCall(int arg)
+            {
+                //MessageBox.Show($"Value Provided From JavaScript: {arg.ToString()}", "C# Method Called");
+                OnTitleSelected?.Invoke(this, arg);
+            }
+        }
+        protected CefSharp.WinForms.ChromiumWebBrowser m_wb;
+#else
         ListView m_lstV;
+#endif
         StatusBar m_sts;
         public SearchPanel(string cnnStr)
         {
@@ -577,6 +653,18 @@ namespace WindowsFormsApp1
                 OnSearch(edt.Text);
             };
 
+#if use_browser
+            var lst = new CefSharp.WinForms.ChromiumWebBrowser("");
+            lst.Dock = DockStyle.Fill;
+            JsHandler jsHandler = new JsHandler();
+            jsHandler.OnTitleSelected += (s, e) =>
+            {
+                Debug.WriteLine("OnSelectTitle {0}", e);
+                OnSelectTitle?.Invoke(s, Convert.ToUInt64(e));
+            };
+            lst.JavascriptObjectRepository.Register("jsHandler", jsHandler, true);
+            m_wb = lst;
+#else
             var lst = new ListView();
             lst.View = View.Details;
             lst.FullRowSelect = true;
@@ -584,17 +672,19 @@ namespace WindowsFormsApp1
             lst.Dock = DockStyle.Fill;
             lst.ItemSelectionChanged += (s, e) =>
               {
-                  UInt64 titleId = 0;
-                  foreach (ListViewItem li in lst.SelectedItems) {
-                      UInt64.TryParse(li.SubItems[1].Text, out titleId);
+                  //UInt64 titleId = 0;
+                  foreach (ListViewItem li in lst.SelectedItems)
+                  {
+                      //UInt64.TryParse(li.SubItems[1].Text, out titleId);
                       //OnSelectTitle(titleId);
-                      OnSelectTitle?.Invoke(s, titleId);
+                      OnSelectTitle?.Invoke(s, Convert.ToUInt64(li.SubItems[1].Tag));
                       break;
                   }
               };
             m_lstV = lst;
+#endif
 
-            var sts = new StatusBar();
+                        var sts = new StatusBar();
             sts.Dock = DockStyle.Bottom;
             sts.ShowPanels = false;
             m_sts = sts;
@@ -602,7 +692,7 @@ namespace WindowsFormsApp1
             int iRow = 0;
             m_tblLayout.RowStyles.Add(new RowStyle(SizeType.AutoSize));
             m_tblLayout.RowStyles.Add(new RowStyle(SizeType.AutoSize));
-            m_tblLayout.RowStyles.Add(new RowStyle(SizeType.Percent,100));
+            m_tblLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 100));
             m_tblLayout.RowStyles.Add(new RowStyle(SizeType.AutoSize));
             m_tblLayout.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
             m_tblLayout.Controls.Add(edt, 0, iRow++);
@@ -614,7 +704,7 @@ namespace WindowsFormsApp1
         public event EventHandler<UInt64> OnSelectTitle;
         private void SelectTitle(int titleId)
         {
-            
+
         }
 
         private void OnSearch(string txt)
@@ -627,11 +717,130 @@ namespace WindowsFormsApp1
             //return;
             var res = srch.Find(txt);
             showSearchRes(res);
-            m_sts.Text = string.Format("elapsed time: {0}(ms)", Environment.TickCount-begin);
+            m_sts.Text = string.Format("elapsed time: {0}(ms)", Environment.TickCount - begin);
+            srch.Close();
         }
 
-        void showSearchRes(SrchRes[] res)
+        public class MarkedTxt
         {
+            public string txt;
+            public int style;   //1->marked 
+        }
+        public class MarkedTitle
+        {
+            public string title;
+            public UInt64 titleId;
+            public List<MarkedTxt> txtLst;
+        }
+
+
+        List<MarkedTitle> convJsData(SrchResult res)
+        {
+            var lst = new List<MarkedTitle>();
+            foreach (var rec in res.recs)
+            {
+                var mt = new MarkedTitle();
+                mt.titleId = rec.detail[0].titleId;
+                mt.title = res.titles.Find((t) => t.ID == mt.titleId).zTitle;
+                //<par,[point]>
+                var tDict = new Dictionary<UInt64, List<int[]>>();
+                var parLst = new List<MyParagraph>();
+                foreach(var w in rec.detail)
+                {
+                    var par = res.paragraphs.Find(p => p.ID == w.parId);
+                    if (tDict.ContainsKey(par.ID))
+                    {
+                        tDict[par.ID].Add(new int[] { w.pos, w.content.Length });
+                        tDict[par.ID].Sort((v1, v2) => v1[0] - v2[0]);
+                    }
+                    else
+                    {
+                        tDict[par.ID] = new List<int[]>
+                        {
+                            new int[] { w.pos, w.content.Length }
+                        };
+                        parLst.Add(par);
+                    }
+                }
+                //split
+                mt.txtLst = new List<MarkedTxt>();
+                foreach (var p in tDict)
+                {
+                    string txt = parLst.Find(par => par.ID == p.Key).content;
+                    int cur = 0;
+                    foreach(var point in p.Value)
+                    {
+                        // txt before [point]
+                        var offset = point[0];
+                        var len = point[1];
+                        if(offset > cur)
+                        {
+                            var n = Math.Min(20, offset - cur);
+                            var pos = txt.IndexOf(' ', offset - n, n);
+                            if (pos != -1) { n = offset - pos; }
+
+                            mt.txtLst.Add(new MarkedTxt()
+                            {
+                                txt = txt.Substring(offset - n, n)
+                            });
+                        }
+                        mt.txtLst.Add(new MarkedTxt()
+                        {
+                            txt = txt.Substring(offset, len),
+                            style = 1
+                        });
+                        cur = offset + len;
+                    }
+                }
+                
+                lst.Add(mt);
+            }
+
+            return lst;
+        }
+
+        string genJsTxt(List<MarkedTitle> lst)
+        {
+            Type[] knownTypes = new Type[] {
+                    typeof(MarkedTxt),
+                    typeof(MarkedTitle),
+                };
+
+            DataContractJsonSerializerSettings settings = new DataContractJsonSerializerSettings
+            {
+                IgnoreExtensionDataObject = true,
+                EmitTypeInformation = EmitTypeInformation.AsNeeded,
+                KnownTypes = knownTypes
+            };
+            var x = new DataContractJsonSerializer(lst.GetType(), settings);
+
+            var mem = new MemoryStream();
+            x.WriteObject(mem, lst);
+            StreamReader sr = new StreamReader(mem);
+            mem.Position = 0;
+            string myStr = sr.ReadToEnd();
+            sr.Dispose();
+            mem.Dispose();
+            return myStr;
+        }
+        private string genHtmlTxt(string jsTxt)
+        {
+            var path = @"D:\tmp\github\utilities\KinhPhat\WindowsFormsApp1\search.html";
+            var txt = File.ReadAllText(path);
+            var htmlTxt = txt.Replace("var jsTxt = null", "var jsTxt = " + jsTxt);
+            return htmlTxt;
+        }
+        void showSearchRes(SrchResult res)
+        {
+#if use_browser
+            //create json data [title, texthtml]
+            var data = convJsData(res);
+            var jsTxt = genJsTxt(data);
+            string htmlTxt = genHtmlTxt(jsTxt);
+            string filename = string.Format(@"{0}{1}", Path.GetTempPath(), "page.htm");
+            File.WriteAllText(filename, htmlTxt);
+            m_wb.Load(filename);
+#else
             var listView1 = m_lstV;
             listView1.Clear();
             listView1.Columns.Add("content");
@@ -639,19 +848,28 @@ namespace WindowsFormsApp1
             listView1.Columns.Add("paragraph");
             listView1.Columns.Add("pos");
             listView1.Columns.Add("diff");
+            listView1.Columns.Add("detail");
             listView1.GridLines = true;
-            foreach (var rec in res)
+            foreach (var rec in res.recs)
             {
                 //var tempTxt = string.Format("{0} {1} {2} {3}", 
                 //    string.Join(" ", rec.path.Select((v)=>v.content).ToArray()),
                 //    rec.d, rec.w.titleId, rec.w.parId);
                 var li = listView1.Items.Add(string.Join(" ", rec.detail.Select((v) => v.content)));
-                li.SubItems.Add(rec.detail[0].titleId.ToString());
-                li.SubItems.Add(string.Join(" ", rec.detail.Select(v=>v.parId)));
+                var titleId = rec.detail[0].titleId;
+                var sub = li.SubItems.Add(res.titles.Find((t) => t.ID == titleId).zTitle);
+                sub.Tag = rec.detail[0].titleId;
+                li.SubItems.Add(string.Join(" ", rec.detail.Select(v => v.parId)));
                 li.SubItems.Add(string.Join(" ", rec.detail.Select(v => v.pos)));
-                var sub = li.SubItems.Add(rec.d.ToString());
-                
+                li.SubItems.Add(rec.d.ToString());
+                var w = rec.detail[0];
+                var parId = w.parId;
+                var parTxt = res.paragraphs.Find((t) => t.ID == parId).content;
+                var txt = parTxt.Substring(0, w.pos) + "[" + w.content + "]" + parTxt.Substring(w.pos + w.content.Length);
+                li.SubItems.Add(parTxt);
             }
+#endif
         }
+
     }
 }
---
+++ KinhPhat/browser_javascript_binding/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.Event;
+using CefSharp.SchemeHandler;
+
+namespace browser_javascript_binding
+{
+    public partial class Form1 : Form
+    {
+        protected CefSharp.WinForms.ChromiumWebBrowser browser;
+       
+        public Form1()
+        {
+            InitializeComponent();
+
+            browser = new CefSharp.WinForms.ChromiumWebBrowser("");
+            browser.Dock = DockStyle.Fill;
+            this.Controls.Add(browser);
+
+            var file = @"D:\tmp\github\utilities\KinhPhat\browser_javascript_binding\simple.html";
+            browser.JavascriptObjectRepository.Register("jsHandler", jsHandler, true);
+            browser.Load(file);
+        }
+
+        JsHandler jsHandler = new JsHandler();
+
+        private void onmyevent(object sender, JavascriptBindingCompleteEventArgs e)
+        {
+           
+            Debug.WriteLine(e);
+        }
+        EventHandler<JavascriptBindingCompleteEventArgs>  eventHandler;
+        class JsHandler
+        {
+            public void HandleJsCall(int arg)
+            {
+                MessageBox.Show($"Value Provided From JavaScript: {arg.ToString()}", "C# Method Called");
+            }
+        }
+    }
+}
